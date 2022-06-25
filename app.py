2dd0ef66f7636ecb0fe0cc0c2b4b652456698d7c
howard halim <halimhoward@gmail.com>
2022-06-26 03:25:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -186,7 +186,7 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-app.config ['SQLALCHEMY_DATABASE_URI'] = 'postgres://wwdvjxfnarrfgi:1ca950510df44652e0ead171affb4ae982837e7deefbaa21efb494462f330a88@ec2-44-205-41-76.compute-1.amazonaws.com:5432/d7hv5jt6s3aits'
+app.config ['SQLALCHEMY_DATABASE_URI'] = "postgres://wwdvjxfnarrfgi:1ca950510df44652e0ead171affb4ae982837e7deefbaa21efb494462f330a88@ec2-44-205-41-76.compute-1.amazonaws.com:5432/d7hv5jt6s3aits"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
