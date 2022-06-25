5ee0444f012de8544924c3423285dd55f9737cea
howard halim <halimhoward@gmail.com>
2022-06-26 03:39:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,55 @@
+from flask import Flask,jsonify,request,make_response
+from flask_sqlalchemy import SQLAlchemy
+from itsdangerous import json
+from pytz import utc
+from datetime import datetime 
+
+app = Flask(__name__)
+
+
+
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello Sammy!'
+
+@app.route('/returnjson', methods = ['GET'])
+def ReturnJSON():
+    if(request.method == 'GET'):
+        data = {
+            "Modules" : 15,
+            "Subject" : "Data Structures and Algorithms",
+        }
+        
+        return jsonify(data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+# app.config ['SQLALCHEMY_DATABASE_URI'] = "postgres://wwdvjxfnarrfgi:1ca950510df44652e0ead171affb4ae982837e7deefbaa21efb494462f330a88@ec2-44-205-41-76.compute-1.amazonaws.com:5432/d7hv5jt6s3aits"
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# db = SQLAlchemy(app)
+
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key = True)
+#     email = db.Column(db.String(100), unique = True, nullable = False)
+#     password = db.Column(db.String(100), nullable = False)
+#     name = db.Column(db.String(100), nullable = False)
+#     passport = db.Column(db.String(100), unique = True, nullable = False)
+#     register = db.Column(db.Integer, nullable = False)
+#     status = db.Column(db.String(100))
+#     eyeCondition = db.Column(db.String(100))
+
+
+
+# class Checkin(db.Model):
+#     id = db.Column(db.Integer, primary_key = True)
+#     time = db.Column(db.DateTime(timezone=True),
+#                            default=datetime.utcnow)
+#     user = db.Column(db.Integer, db.ForeignKey('user.id'))
+#     place = db.Column(db.String(100))
 
 
 # app =   Flask(__name__)
@@ -177,53 +229,3 @@
 # #         data.append(row.firstname)
 # #     print(jsonify(data))
 # #     return make_response(json.dumps(data),200)
-
-from flask import Flask,jsonify,request,make_response
-from flask_sqlalchemy import SQLAlchemy
-from itsdangerous import json
-from pytz import utc
-from datetime import datetime 
-
-app = Flask(__name__)
-
-# app.config ['SQLALCHEMY_DATABASE_URI'] = "postgres://wwdvjxfnarrfgi:1ca950510df44652e0ead171affb4ae982837e7deefbaa21efb494462f330a88@ec2-44-205-41-76.compute-1.amazonaws.com:5432/d7hv5jt6s3aits"
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# db = SQLAlchemy(app)
-
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key = True)
-#     email = db.Column(db.String(100), unique = True, nullable = False)
-#     password = db.Column(db.String(100), nullable = False)
-#     name = db.Column(db.String(100), nullable = False)
-#     passport = db.Column(db.String(100), unique = True, nullable = False)
-#     register = db.Column(db.Integer, nullable = False)
-#     status = db.Column(db.String(100))
-#     eyeCondition = db.Column(db.String(100))
-
-
-
-# class Checkin(db.Model):
-#     id = db.Column(db.Integer, primary_key = True)
-#     time = db.Column(db.DateTime(timezone=True),
-#                            default=datetime.utcnow)
-#     user = db.Column(db.Integer, db.ForeignKey('user.id'))
-#     place = db.Column(db.String(100))
-
-
-
-@app.route('/')
-def hello_world():
-    return 'Hello Sammy!'
-
-@app.route('/returnjson', methods = ['GET'])
-def ReturnJSON():
-    if(request.method == 'GET'):
-        data = {
-            "Modules" : 15,
-            "Subject" : "Data Structures and Algorithms",
-        }
-        
-        return jsonify(data)
-
-if __name__ == "__main__":
-    app.run(debug=True)
