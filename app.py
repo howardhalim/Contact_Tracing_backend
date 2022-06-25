123b1c5571eff05b46fd63f1a4aa65fdf2e226e9
howard halim <halimhoward@gmail.com>
2022-06-26 03:44:42 +0800

    update

--- app.py
+++ app.py
@@ -6,8 +6,28 @@ from datetime import datetime
 
 app = Flask(__name__)
 
+app.config ['SQLALCHEMY_DATABASE_URI'] = "postgres://wwdvjxfnarrfgi:1ca950510df44652e0ead171affb4ae982837e7deefbaa21efb494462f330a88@ec2-44-205-41-76.compute-1.amazonaws.com:5432/d7hv5jt6s3aits"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    email = db.Column(db.String(100), unique = True, nullable = False)
+    password = db.Column(db.String(100), nullable = False)
+    name = db.Column(db.String(100), nullable = False)
+    passport = db.Column(db.String(100), unique = True, nullable = False)
+    register = db.Column(db.Integer, nullable = False)
+    status = db.Column(db.String(100))
+    eyeCondition = db.Column(db.String(100))
 
 
+
+class Checkin(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    time = db.Column(db.DateTime(timezone=True),
+                           default=datetime.utcnow)
+    user = db.Column(db.Integer, db.ForeignKey('user.id'))
+    place = db.Column(db.String(100))
 
 
 @app.route('/')
